880fe8e7b3e567f93a9d4119351777a701056721
minly <dolphinwang999@yahoo.com>
2016-05-07 12:26:15 +0800

    init

---
+++ spring-session-basic/src/main/java/com/github/minly/session/basic/DemoApplication.java
@@ -0,0 +1,32 @@
+package com.github.minly.session.basic;
+
+import java.util.UUID;
+
+import javax.servlet.http.HttpSession;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.session.data.redis.config.annotation.web.http.EnableRedisHttpSession;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@EnableRedisHttpSession  // will look for the RedisConnectionFactory that Boot provides
+@SpringBootApplication
+@RestController
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DemoApplication.class, args);
+	}
+
+	@RequestMapping("/")
+	public String hello(HttpSession session) {
+		UUID uid = (UUID) session.getAttribute("uid");
+		if (uid == null) {
+			uid = UUID.randomUUID();
+		}
+		session.setAttribute("uid", uid);
+		return uid.toString();
+	}
+
+}
